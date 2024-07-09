188280958cec2764e7852ea56de4e193874b97b5
Yanall-Boutros <YanallBoutros@Protonmail.com>
2024-07-08 23:37:41 -0700

    Removed custom transformer for torch default transformer

--- scrape.py
+++ scrape.py
@@ -20,379 +20,6 @@ tok = Tokenizer(WordPiece())
 trainer = WordPieceTrainer()#special_tokens=["[BOS]", "[EOS]"])
 tok.pre_tokenizer = Whitespace()
 
-class Embedding(nn.Module):
-    def __init__(self, vocab_size, embed_dim):
-        """
-        Args:
-            vocab_size: size of vocabulary
-            embed_dim: dimension of embeddings
-        """
-        super(Embedding, self).__init__()
-        self.embed = nn.Embedding(vocab_size, embed_dim)
-    def forward(self, x):
-        """
-        Args:
-            x: input vector
-        Returns:
-            out: embedding vector
-        """
-        out = self.embed(x)
-        return out
-
-class PositionalEmbedding(nn.Module):
-    def __init__(self,max_seq_len,embed_model_dim):
-        """
-        Args:
-            seq_len: length of input sequence
-            embed_model_dim: demension of embedding
-        """
-        super(PositionalEmbedding, self).__init__()
-        self.embed_dim = embed_model_dim
-
-        pe = torch.zeros(max_seq_len,self.embed_dim)
-        for pos in range(max_seq_len):
-            for i in range(0,self.embed_dim,2):
-                # ToDO: maybe change 10000 to 500 since Jazz 1460 context length much smaller for positional encoding
-                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i)/self.embed_dim)))
-                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1))/self.embed_dim)))
-        pe = pe.unsqueeze(0)
-        self.register_buffer('pe', pe)
-
-
-    def forward(self, x):
-        """
-        Args:
-            x: input vector
-        Returns:
-            x: output
-        """
-      
-        # make embeddings relatively larger
-        x = x * math.sqrt(self.embed_dim)
-        #add constant to embedding
-        seq_len = x.size(1)
-        x = x + torch.autograd.Variable(self.pe[:,:seq_len], requires_grad=False)
-        return x
-
-class MultiHeadAttention(nn.Module):
-    def __init__(self, embed_dim=512, n_heads=8):
-        """
-        Args:
-            embed_dim: dimension of embeding vector output
-            n_heads: number of self attention heads
-        """
-        super(MultiHeadAttention, self).__init__()
-
-        self.embed_dim = embed_dim    #512 dim
-        self.n_heads = n_heads   #8
-        self.single_head_dim = int(self.embed_dim / self.n_heads)   #512/8 = 64  . each key,query, value will be of 64d
-
-        #key,query and value matrixes    #64 x 64
-        self.query_matrix = nn.Linear(self.single_head_dim , self.single_head_dim ,bias=False)  # single key matrix for all 8 keys #512x512
-        self.key_matrix = nn.Linear(self.single_head_dim  , self.single_head_dim, bias=False)
-        self.value_matrix = nn.Linear(self.single_head_dim ,self.single_head_dim , bias=False)
-        self.out = nn.Linear(self.n_heads*self.single_head_dim ,self.embed_dim)
-
-    def forward(self,key,query,value,mask=None):    #batch_size x sequence_length x embedding_dim    # 32 x 10 x 512
-
-        """
-        Args:
-           key : key vector
-           query : query vector
-           value : value vector
-           mask: mask for decoder
-
-        Returns:
-           output vector from multihead attention
-        """
-        batch_size = key.size(0)
-        seq_length = key.size(1)
-
-        # query dimension can change in decoder during inference.
-        # so we cant take general seq_length
-        seq_length_query = query.size(1)
-
-        # 32x10x512
-        key = key.view(batch_size, seq_length, self.n_heads, self.single_head_dim)  #batch_size x sequence_length x n_heads x single_head_dim = (32x10x8x64)
-        query = query.view(batch_size, seq_length_query, self.n_heads, self.single_head_dim) #(32x10x8x64)
-        value = value.view(batch_size, seq_length, self.n_heads, self.single_head_dim) #(32x10x8x64)
-
-        k = self.key_matrix(key)       # (32x10x8x64)
-        q = self.query_matrix(query)
-        v = self.value_matrix(value)
-
-        q = q.transpose(1,2)  # (batch_size, n_heads, seq_len, single_head_dim)    # (32 x 8 x 10 x 64)
-        k = k.transpose(1,2)  # (batch_size, n_heads, seq_len, single_head_dim)
-        v = v.transpose(1,2)  # (batch_size, n_heads, seq_len, single_head_dim)
-
-        # computes attention
-        # adjust key for matrix multiplication
-        k_adjusted = k.transpose(-1,-2)  #(batch_size, n_heads, single_head_dim, seq_ken)  #(32 x 8 x 64 x 10)
-        product = torch.matmul(q, k_adjusted)  #(32 x 8 x 10 x 64) x (32 x 8 x 64 x 10) = #(32x8x10x10)
-
-
-        # fill those positions of product matrix as (-1e20) where mask positions are 0
-        if mask is not None:
-             product = product.masked_fill(mask == 0, float("-1e20"))
-
-        #divising by square root of key dimension
-        product = product / math.sqrt(self.single_head_dim) # / sqrt(64)
-
-        #applying softmax
-        scores = F.softmax(product, dim=-1)
-
-        #mutiply with value matrix
-        scores = torch.matmul(scores, v)  ##(32x8x 10x 10) x (32 x 8 x 10 x 64) = (32 x 8 x 10 x 64)
-
-        #concatenated output
-        concat = scores.transpose(1,2).contiguous().view(batch_size, seq_length_query, self.single_head_dim*self.n_heads)  # (32x8x10x64) -> (32x10x8x64)  -> (32,10,512)
-
-        output = self.out(concat) #(32,10,512) -> (32,10,512)
-
-        return output
-
-class TransformerBlock(nn.Module):
-    def __init__(self, embed_dim, expansion_factor=4, n_heads=8):
-        super(TransformerBlock, self).__init__()
-        
-        """
-        Args:
-           embed_dim: dimension of the embedding
-           expansion_factor: fator ehich determines output dimension of linear layer
-           n_heads: number of attention heads
-        
-        """
-        self.attention = MultiHeadAttention(embed_dim, n_heads)
-        
-        self.norm1 = nn.LayerNorm(embed_dim) 
-        self.norm2 = nn.LayerNorm(embed_dim)
-        
-        self.feed_forward = nn.Sequential(
-                          nn.Linear(embed_dim, expansion_factor*embed_dim),
-                          nn.ReLU(),
-                          nn.Linear(expansion_factor*embed_dim, embed_dim)
-        )
-
-        self.dropout1 = nn.Dropout(0.2)
-        self.dropout2 = nn.Dropout(0.2)
-
-    def forward(self,key,query,value):
-        
-        """
-        Args:
-           key: key vector
-           query: query vector
-           value: value vector
-           norm2_out: output of transformer block
-        
-        """
-        
-        attention_out = self.attention(key,query,value)  #32x10x512
-        attention_residual_out = attention_out + value  #32x10x512
-        norm1_out = self.dropout1(self.norm1(attention_residual_out)) #32x10x512
-
-        feed_fwd_out = self.feed_forward(norm1_out) #32x10x512 -> #32x10x2048 -> 32x10x512
-        feed_fwd_residual_out = feed_fwd_out + norm1_out #32x10x512
-        norm2_out = self.dropout2(self.norm2(feed_fwd_residual_out)) #32x10x512
-
-        return norm2_out
-
-
-
-class TransformerEncoder(nn.Module):
-    """
-    Args:
-        seq_len : length of input sequence
-        embed_dim: dimension of embedding
-        num_layers: number of encoder layers
-        expansion_factor: factor which determines number of linear layers in feed forward layer
-        n_heads: number of heads in multihead attention
-        
-    Returns:
-        out: output of the encoder
-    """
-    def __init__(self, seq_len, vocab_size, embed_dim, num_layers=2, expansion_factor=4, n_heads=8):
-        super(TransformerEncoder, self).__init__()
-        
-        self.embedding_layer = Embedding(vocab_size, embed_dim)
-        self.positional_encoder = PositionalEmbedding(seq_len, embed_dim)
-
-        self.layers = nn.ModuleList([TransformerBlock(embed_dim, expansion_factor, n_heads) for i in range(num_layers)])
-    
-    def forward(self, x):
-        embed_out = self.embedding_layer(x)
-        out = self.positional_encoder(embed_out)
-        for layer in self.layers:
-            out = layer(out,out,out)
-
-        return out  #32x10x512
-
-class DecoderBlock(nn.Module):
-    def __init__(self, embed_dim, expansion_factor=4, n_heads=8):
-        super(DecoderBlock, self).__init__()
-
-        """
-        Args:
-           embed_dim: dimension of the embedding
-           expansion_factor: fator ehich determines output dimension of linear layer
-           n_heads: number of attention heads
-        
-        """
-        self.attention = MultiHeadAttention(embed_dim, n_heads=8)
-        self.norm = nn.LayerNorm(embed_dim)
-        self.dropout = nn.Dropout(0.2)
-        self.transformer_block = TransformerBlock(embed_dim, expansion_factor, n_heads)
-        
-    
-    def forward(self, key, query, x,mask):
-        
-        """
-        Args:
-           key: key vector
-           query: query vector
-           value: value vector
-           mask: mask to be given for multi head attention 
-        Returns:
-           out: output of transformer block
-    
-        """
-        
-        #we need to pass mask mask only to fst attention
-        attention = self.attention(x,x,x,mask=mask) #32x10x512
-        value = self.dropout(self.norm(attention + x))
-        
-        out = self.transformer_block(key, query, value)
-
-        
-        return out
-
-
-class TransformerDecoder(nn.Module):
-    def __init__(self, target_vocab_size, embed_dim, seq_len, num_layers=2, expansion_factor=4, n_heads=8):
-        super(TransformerDecoder, self).__init__()
-        """  
-        Args:
-           target_vocab_size: vocabulary size of taget
-           embed_dim: dimension of embedding
-           seq_len : length of input sequence
-           num_layers: number of encoder layers
-           expansion_factor: factor which determines number of linear layers in feed forward layer
-           n_heads: number of heads in multihead attention
-        
-        """
-        self.word_embedding = nn.Embedding(target_vocab_size, embed_dim)
-        self.position_embedding = PositionalEmbedding(seq_len, embed_dim)
-
-        self.layers = nn.ModuleList(
-            [
-                DecoderBlock(embed_dim, expansion_factor=4, n_heads=8) 
-                for _ in range(num_layers)
-            ]
-
-        )
-        self.fc_out = nn.Linear(embed_dim, target_vocab_size)
-        self.dropout = nn.Dropout(0.2)
-
-    def forward(self, x, enc_out, mask):
-        
-        """
-        Args:
-            x: input vector from target
-            enc_out : output from encoder layer
-            trg_mask: mask for decoder self attention
-        Returns:
-            out: output vector
-        """
-            
-        
-        x = self.word_embedding(x)  #32x10x512
-        x = self.position_embedding(x) #32x10x512
-        x = self.dropout(x)
-     
-        for layer in self.layers:
-            x = layer(enc_out, x, enc_out, mask) 
-
-        out = F.softmax(self.fc_out(x))
-
-        return out
-
-class Transformer(nn.Module):
-    def __init__(self, embed_dim, src_vocab_size, target_vocab_size, seq_length,num_layers=2, expansion_factor=4, n_heads=8):
-        super(Transformer, self).__init__()
-        
-        """  
-        Args:
-           embed_dim:  dimension of embedding 
-           src_vocab_size: vocabulary size of source
-           target_vocab_size: vocabulary size of target
-           seq_length : length of input sequence
-           num_layers: number of encoder layers
-           expansion_factor: factor which determines number of linear layers in feed forward layer
-           n_heads: number of heads in multihead attention
-        
-        """
-        
-        self.target_vocab_size = target_vocab_size
-
-        self.encoder = TransformerEncoder(seq_length, src_vocab_size, embed_dim, num_layers=num_layers, expansion_factor=expansion_factor, n_heads=n_heads)
-        self.decoder = TransformerDecoder(target_vocab_size, embed_dim, seq_length, num_layers=num_layers, expansion_factor=expansion_factor, n_heads=n_heads)
-        
-    
-    def make_trg_mask(self, trg):
-        """
-        Args:
-            trg: target sequence
-        Returns:
-            trg_mask: target mask
-        """
-        batch_size, trg_len = trg.shape
-        # returns the lower triangular part of matrix filled with ones
-        trg_mask = torch.tril(torch.ones((trg_len, trg_len))).expand(
-            batch_size, 1, trg_len, trg_len
-        ).cuda()
-        return trg_mask    
-
-    def decode(self,src,trg):
-        """
-        for inference
-        Args:
-            src: input to encoder 
-            trg: input to decoder
-        out:
-            out_labels : returns final prediction of sequence
-        """
-        trg_mask = self.make_trg_mask(trg)
-        enc_out = self.encoder(src)
-        out_labels = []
-        batch_size,seq_len = src.shape[0],src.shape[1]
-        #outputs = torch.zeros(seq_len, batch_size, self.target_vocab_size)
-        out = trg
-        for i in range(seq_len): #10
-            out = self.decoder(out,enc_out,trg_mask) #bs x seq_len x vocab_dim
-            # taking the last token
-            out = out[:,-1,:]
-     
-            out = out.argmax(-1)
-            out_labels.append(out.item())
-            out = torch.unsqueeze(out,axis=0)
-          
-        
-        return out_labels
-    
-    def forward(self, src, trg):
-        """
-        Args:
-            src: input to encoder 
-            trg: input to decoder
-        out:
-            out: final vector which returns probabilities of each target word
-        """
-        trg_mask = self.make_trg_mask(trg)
-        enc_out = self.encoder(src)
-   
-        outputs = self.decoder(trg, enc_out, trg_mask)
-        return outputs
-
 # Useful for getting sense of unique chord types (~1000), max length of measures (122)
 def ireal_set_add(tunes, trgt_set):
     measure_len_to_tunes = {}
@@ -461,43 +88,71 @@ def get_corpus(tunes):
             else: hist[char] = 1
     return rows
 
+
+def embedding_to_token(embedding, embedding_layer):
+  """
+  Finds the closest token in the embedding layer to the input embedding.
+
+  Args:
+      embedding: Input embedding vector (tensor).
+      embedding_layer: Embedding layer (nn.Embedding module).
+
+  Returns:
+      token: The closest token (string) based on cosine similarity.
+  """
+  # Get all embedding weights from the layer
+  embedding_weights = embedding_layer.weight.detach()  # Detach weights to avoid gradients
+
+  # Calculate cosine similarity between input embedding and all weights
+  similarities = torch.nn.functional.cosine_similarity(embedding.unsqueeze(0), embedding_weights)
+
+  # Get the index of the most similar embedding
+  most_similar_index = torch.argmax(similarities).item()
+  token = tok.id_to_token(most_similar_index)
+  # Retrieve the token based on the index (assuming vocabulary order matches weight order)
+  return token
+
 def main():
     device = torch.device('cuda')
     chord_types = set()
     with open("./ireal_url", "r") as f: tunes = Tune.parse_ireal_url(f.read())
     measure_len_to_tunes, measures = ireal_set_add(tunes, chord_types)    
-    #tok.add_tokens(list(chord_types))
-    tok.add_special_tokens(["*A", "*B", "*C", "*D", "{", "}", "(", ")", "<", ">", "T34", "T44", "T64", "T54", "T12", "T22"])
+    tok.add_tokens(list(chord_types))
+    tok.add_special_tokens(["*A", "*B", "*C", "*D", "{", "}", "(", ")", "<", ">", "T34", "T44", "T64", "T54", "T12", "T22", "[BOS]", "[EOS]"])
     corpus =  get_corpus(tunes)
     tok.train_from_iterator(corpus, trainer=trainer)
     src_vocab_size = target_vocab_size = tok.get_vocab_size()
     seq_length = 256
-    num_layers = 6
-    model = Transformer(embed_dim=512, src_vocab_size=src_vocab_size, 
-                    target_vocab_size=target_vocab_size, seq_length=seq_length,
-                    num_layers=num_layers, expansion_factor=4, n_heads=8) 
+    num_layers = 2
+    Embedding = torch.nn.Embedding(src_vocab_size, 256)
+    Embedding.cuda()
+    model = torch.nn.Transformer(d_model=256)
+    #model = Transformer(embed_dim=128, src_vocab_size=src_vocab_size, 
+    #                target_vocab_size=target_vocab_size, seq_length=seq_length,
+    #                num_layers=num_layers, expansion_factor=2, n_heads=2) 
     model.cuda()
-    criterion = torch.nn.NLLLoss()  # ignore padding token
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    #criterion = torch.nn.NLLLoss()  # ignore padding token
+    #criterion = torch.nn.CrossEntropyLoss()
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
     num_epochs = 5
     for epoch in range(num_epochs):
-        for row in tok.encode_batch(corpus):
-            #for sub_phrase_end_index in range(1, len(row)-1):
-            for roll_i in range(len(row)):
-                optimizer.zero_grad()
-#                src = Tensor(row.ids).type(torch.int32).unsqueeze(0)
-                src = Tensor(row.ids).type(torch.int64)#.unsqueeze(0)
-#                trg = Tensor(row.ids[:sub_phrase_end_index]).type(torch.int32).roll(roll_i).unsqueeze(0)
-                trg = src.roll(roll_i)
-                src = src.unsqueeze(0).cuda()
-                trg = trg.unsqueeze(0).cuda()
-                outputs = model(src, trg)
-                loss = criterion(outputs.view(-1, model.target_vocab_size), trg.contiguous().view(-1))
+        model.train()
+        optimizer.zero_grad()
+        for i, row in enumerate(tok.encode_batch(corpus)):
+            print(f"Epoch: {i/len(corpus)}%")
+            for sub_phrase_end_index in range(1, len(row)-1):
+                src = Tensor(row.ids).type(torch.int64).cuda()#.unsqueeze(0)
+                trg = Tensor(row.ids).type(torch.int64)[sub_phrase_end_index:].cuda()#src.roll(roll_i).cuda()
+                src_emb = Embedding(src).cuda()
+                trg_emb = Embedding(trg).cuda()
+                outputs = model(src_emb, trg_emb)
+                #loss = criterion(outputs.view(-1, 256), trg_emb.view(-1, 256))
+                #pdb.set_trace()
+                loss = torch.nn.functional.cross_entropy(outputs.view(-1, 256), trg_emb.view(-1, 256))
                 loss.backward()
-                step = optimizer.step()
-                print(f"Epoch: {epoch+1}/{num_epochs}, Step: {step}, Loss: {loss.item()}")
-                #loss = criterion(outputs.view(-1, model.target_vocab_size), trg.contiguous.view(-1))
-
+                optimizer.step()
+                print(f"Epoch: {epoch+1}/{num_epochs}, Loss: {loss.item()}")
     pdb.set_trace()
 
 if __name__ == "__main__": main()
