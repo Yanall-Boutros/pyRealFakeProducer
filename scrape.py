e83ba37afd04eee88a6cf1a9bcc5c46f41f0e739
Yanall-Boutros <YanallBoutros@Protonmail.com>
2024-08-04 15:46:04 -0700

    Replaced chord strings with measure strings, forced tokenization down to the chord level, reducing the vocab size from 30000 to 4000

--- scrape.py
+++ scrape.py
@@ -21,35 +21,9 @@ trainer = BpeTrainer()#special_tokens=["<BOS>", "<EOS>"])
 #trainer = WordPieceTrainer()#special_tokens=["<BOS>", "<EOS>"])
 #tok.pre_tokenizer = Whitespace()
 from rotary_embedding_torch import RotaryEmbedding
+import pytorch_model_classes as pmc
 
-class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=256):
-        super().__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(512.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
-
-def get_corpus(tunes):
-    hist = {}
-    rows = []
-    for tune in tunes:
-        rows.append(f"%BOS% {tune.chord_string} %EOS%")
-        #rows.append(tune.chord_string)
-        for char in tune.chord_string:
-            if char in hist: hist[char] += 1
-            else: hist[char] = 1
-    return rows
-
+def get_corpus(tunes): return [["%BOS%"]+tune.measures_as_strings+["%EOS%"] for tune in tunes]
 
 def embedding_to_token(embedding, embedding_layer):
   """
@@ -66,7 +40,7 @@ def embedding_to_token(embedding, embedding_layer):
   embedding_weights = embedding_layer.weight.detach()  # Detach weights to avoid gradients
 
   # Calculate cosine similarity between input embedding and all weights
-  similarities = torch.nn.functional.cosine_similarity(embedding.unsqueeze(0), embedding_weights)
+  similarities = F.cosine_similarity(embedding.unsqueeze(0), embedding_weights)
 
   # Get the index of the most similar embedding
   most_similar_index = torch.argmax(similarities).item()
@@ -75,81 +49,84 @@ def embedding_to_token(embedding, embedding_layer):
   return token
 
 
-def generate_start_with(tokens, model, Embedding, PosEnc, tok):
+def generate_start_with(tokens, model, Embedding, tok):
     vocab = tok.get_vocab()
-    #pdb.set_trace()
     tok_history = []
     tok_history.extend(tokens)
     print(tokens)
     timeout = 0
-    #pdb.set_trace()
     while all([token in vocab for token in tok_history]) and len(tok_history) < 12:
       embedding = Embedding(Tensor([tok.token_to_id(token) for token in tok_history]).type(torch.int64).cuda())
-      next_tokens = [embedding_to_token(n_emb, Embedding) for n_emb in model.decoder(Embedding(Tensor([tok.token_to_id(token) for token in tok_history]).type(torch.int64).cuda()), model.encoder(Embedding(Tensor([tok.token_to_id(token) for token in tok_history]).type(torch.int64).cuda()))) ]
-      tok_history.append(next_tokens[0])
-      if "%EOS%" in tok_history:
-          print("Pred: ", tok_history)
-          return
+      next_tokens = [embedding_to_token(n_emb, Embedding) for n_emb in model.decoder(embedding, model.encoder(embedding)) ]
+      if next_tokens:
+          if next_tokens[0]:
+              tok_history.append(next_tokens[0])
+              if "%EOS%" in tok_history or "%EOS%" in next_tokens[0]:
+                  print("Pred: ", tok_history)
+                  return
+          else: return
+      else: return
     print("Pred: ", tok_history)
 
 def beam_search(inp, model, emb, tok, num_beams=10):
     beams = num_beams*[(inp, 1.0)]
 
-def main():
+def init():
     max_seq_len = 256
-    device = torch.device('cuda')
-    chord_types = set()
+    emb_dim = 64
     with open("./ireal_url", "r") as f: tunes = Tune.parse_ireal_url(f.read())
+    chord_types = set()
     ireal_set_add(tunes, chord_types)
-    for tune in tunes: tok.add_tokens(list(tune.measures_as_strings)) 
-    tok.add_special_tokens(["%BOS%", "%EOS%", "%PAD%"])#"*A", "*B", "*C", "*D", "{", "}", "(", ")", "<", ">", "T34", "T44", "T64", "T54", "T12", "T22", "[BOS]", "[EOS]", "[PAD]" ])
-    tok.add_tokens(list(chord_types))
     corpus =  get_corpus(tunes)
+    #for tune in tunes: tok.add_tokens(list(tune.measures_as_strings)) 
+    tok.add_special_tokens(["%BOS%", "%EOS%"])# "%PAD%"])#"*A", "*B", "*C", "*D", "{", "}", "(", ")", "<", ">", "T34", "T44", "T64", "T54", "T12", "T22", "[BOS]", "[EOS]", "[PAD]" ])
+    pdb.set_trace()
+    #tok.add_tokens(list(chord_types))
     tok.train_from_iterator(corpus, trainer=trainer)
     src_vocab_size = target_vocab_size = tok.get_vocab_size()
-    emb_dim = 64
-    Embedding = torch.nn.Embedding(src_vocab_size, emb_dim)
-    #RotEmbedding = RotaryEmbedding(emb_dim)
-    Embedding.cuda()
-    PosEnc = PositionalEncoding(emb_dim).cuda()
-    #RotEmbedding.cuda()
-    model = torch.nn.Transformer(d_model=emb_dim, batch_first=True, dim_feedforward=2**8, dropout=0.1, nhead=4, num_encoder_layers=8, num_decoder_layers=8, norm_first=False, bias=True)
-    #mh_attention = model.encoder.layers[0].self_attn
-    #model.encoder.layers[0].self_attn = RotaryAttention(mh_attention, RotEmbedding).cuda()
-    model.cuda()
-    params = list(model.parameters())+list(Embedding.parameters()) + list(PosEnc.parameters())
-    optimizer = torch.optim.Adam(params, lr=0.00001)# + list(RotEmbedding.parameters()), lr=0.0001)
     num_epochs = 5
+    return max_seq_len, torch.device('cuda'), chord_types, tunes, tok, src_vocab_size,corpus, emb_dim, num_epochs
+
+def main():
+    max_seq_len, device, chord_types, tunes, tok, vocab_size, corpus, emb_dim, num_epochs = init()
+    Embedding    = torch.nn.Embedding(vocab_size, emb_dim).cuda()
+    #RotEmbedding = RotaryEmbedding(emb_dim).cuda()
+    PosEnc = pmc.PositionalEncoding(emb_dim).cuda()
+    model = torch.nn.Transformer(d_model=emb_dim, batch_first=True, dim_feedforward=2**8, dropout=0.1, nhead=4, num_encoder_layers=8, num_decoder_layers=8, norm_first=False, bias=True).cuda()
+    #model_test = pmc.Transformer(emb_dim, 4, 8, 12, 2**10, vocab_size)
+    params = list(model.parameters())+list(Embedding.parameters()) + list(PosEnc.parameters())# + list(RotEmbedding.parameters()) #+ list(#PosEnc.parameters())
+    optimizer = torch.optim.Adam(params, lr=0.0001)
+    pdb.set_trace()
     for epoch in range(num_epochs):
-        #pdb.set_trace()
-        for i, row in enumerate(tok.encode_batch(corpus)):
-#            print("Source/Target: ", [tok.id_to_token(id) for id in row.ids])
-#            sss is split_sub_string, and is an index to split a target phrase in 2. The first part is the context, the last part is what should be predicted
+        for i, rows in enumerate(corpus):
+            row = [tok.encode(r[0]).ids[0] for r in rows]
             model.train()
-            for sss in range(len(row.ids)-1, len(row.ids)):
+            for sss in range(len(row)-1, len(row)):
                 if (sss%100 == 0): print(f"Epoch: {100*(i/len(corpus))}%")
-
-                src = torch.concat((Tensor(row.ids).type(torch.int64).cuda()[:sss-1], Tensor((max_seq_len-(sss-1))*[tok.token_to_id("%EOS%")]).type(torch.int64).cuda()))
-                trg = torch.concat(
-                    (Tensor(row.ids).type(torch.int64).cuda()[:sss], Tensor((max_seq_len-(sss))*[tok.token_to_id("%EOS%")]).type(torch.int64).cuda()))#sub_phrase_end_index:].cuda()#src.roll(roll_i).cuda()
-
+                src = torch.concat((
+                    Tensor(row).type(torch.int64).cuda()[:sss-1],
+                    Tensor((max_seq_len-(sss-1))*[tok.token_to_id("%EOS%")]).type(torch.int64).cuda()
+                ))
+                trg = torch.concat((
+                    Tensor(row).type(torch.int64).cuda()[:sss],
+                    Tensor((max_seq_len-(sss))*[tok.token_to_id("%EOS%")]).type(torch.int64).cuda()
+                ))# Teacher Forcing: #sub_phrase_end_index:].cuda()#src.roll(roll_i).cuda()
+                tgt_mask = torch.triu(torch.ones(trg.size(0), trg.size(0)), diagonal=1).bool()
                 optimizer.zero_grad()
                 src_emb = Embedding(src).cuda()
                 trg_emb = Embedding(trg).cuda()
-                src_emb = PosEnc(src_emb)
-                trg_emb = PosEnc(trg_emb)
-                #src_emb = RotEmbedding(src_emb)
-                #trg_emb = RotEmbedding(trg_emb)
-                outputs = model(src_emb, trg_emb)
-                #loss = criterion(outputs.view(-1, 256), trg_emb.view(-1, 256))
-                #pdb.set_trace()
-                loss = torch.nn.functional.cross_entropy(outputs.view(-1, emb_dim), trg_emb.view(-1, emb_dim))
+                #src_emb = PosEnc(Embedding(src).cuda())
+                #trg_emb = PosEnc(Embedding(trg).cuda())
+                outputs = model(src_emb, trg_emb, src_is_causal=True, tgt_is_causal=True,
+                                src_mask=model.generate_square_subsequent_mask(src.size(0)),
+                                tgt_mask=model.generate_square_subsequent_mask(trg.size(0)))
+                loss = F.cross_entropy(outputs.view(-1, emb_dim), trg_emb.view(-1, emb_dim))
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(params, 1)
                 optimizer.step()
                 print(f"Epoch: {epoch+1}/{num_epochs}, Loss: {loss.item()}")
             model.eval()
-            generate_start_with(["%BOS%", "A-"], model, Embedding, PosEnc, tok)
+            generate_start_with(["%BOS%"], model, Embedding, tok)
         torch.save(model, f"{epoch}.pt")
         pdb.set_trace()
 
