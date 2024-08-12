fd1b7915507bc699d186a67926a294107299c650
= <=>
2024-08-12 16:09:30 -0700

    fix: removed unused model

--- pytorch_model_classes.py
+++ pytorch_model_classes.py
@@ -8,48 +8,6 @@ import numpy as np
 from torch import Tensor
 from utils import ireal_set_add
 from rotary_embedding_torch import RotaryEmbedding
-class CausalSelfAttention(nn.Module):
-    def __init__(self, num_heads: int, embed_dimension: int, bias: bool=False, is_causal: bool=False, dropout:float=0.0):
-        super().__init__()
-        assert embed_dimension % num_heads == 0
-        # key, query, value projections for all heads, but in a batch
-        self.c_attn = nn.Linear(embed_dimension, 3 * embed_dimension, bias=bias)
-        # output projection
-        self.c_proj = nn.Linear(embed_dimension, embed_dimension, bias=bias)
-        # regularization
-        self.dropout = dropout
-        self.resid_dropout = nn.Dropout(dropout)
-        self.num_heads = num_heads
-        self.embed_dimension = embed_dimension
-        # Perform causal masking
-        self.is_causal = is_causal
-
-    def forward(self, x):
-        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        query_projected = self.c_attn(x)
-
-        batch_size = query_projected.size(0)
-        embed_dim = query_projected.size(2)
-        head_dim = embed_dim // (self.num_heads * 3)
-
-        query, key, value = query_projected.chunk(3, -1)
-        query = query.view(batch_size, -1, self.num_heads, head_dim).transpose(1, 2)
-        key = key.view(batch_size, -1, self.num_heads, head_dim).transpose(1, 2)
-        value = value.view(batch_size, -1, self.num_heads, head_dim).transpose(1, 2)
-
-        if self.training:
-            dropout = self.dropout
-            is_causal = self.is_causal
-        else:
-            dropout = 0.0
-            is_causal = False
-
-        y = F.scaled_dot_product_attention(query, key, value, attn_mask=None, dropout_p=dropout, is_causal=is_causal)
-        y = y.transpose(1, 2).view(batch_size, -1, self.num_heads * head_dim)
-
-        y = self.resid_dropout(self.c_proj(y))
-        return y
-
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout=0.1, max_len=256):
         super(PositionalEncoding, self).__init__()
@@ -66,7 +24,7 @@ class PositionalEncoding(nn.Module):
         x = x + self.pe[:x.size(0)]
         return self.dropout(x)
 
-class Transformer(nn.Module):
+class Transformer_1(nn.Module):
     def __init__(self, d_model, nhead, num_encoder_layers, num_decoder_layers, dim_feedforward, vocab_size, dropout=0.1):  
         super().__init__()
         self.encoder = nn.TransformerEncoder(
@@ -82,18 +40,18 @@ class Transformer(nn.Module):
         self.tgt_embed = nn.Embedding(vocab_size, d_model)
 
         # Positional encoding (example)
-        self.pos_encoder = RotaryEmbedding(d_model)#PositionalEncoding(d_model, dropout)
+        #self.pos_encoder = RotaryEmbedding(d_model)#PositionalEncoding(d_model, dropout)
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
 
         # Output layer (example)
         self.generator = nn.Linear(d_model, vocab_size)
 
     def forward(self, src, tgt, src_mask=None, tgt_mask=None, src_padding_mask=None, tgt_padding_mask=None):
-        #src = self.src_embed(src) * math.sqrt(self.d_model)
         src = self.src_embed(src)
         src = self.pos_encoder(src)
         memory = self.encoder(src) #src_mask=src_mask, src_key_padding_mask=src_padding_mask)
 
-        tgt = self.tgt_embed(tgt) #* math.sqrt(self.d_model)
+        tgt = self.tgt_embed(tgt)
         tgt = self.pos_encoder(tgt)
         output = self.decoder(tgt, memory, tgt_mask=tgt_mask, memory_mask=None, tgt_key_padding_mask=tgt_padding_mask, memory_key_padding_mask=src_padding_mask)  
 
